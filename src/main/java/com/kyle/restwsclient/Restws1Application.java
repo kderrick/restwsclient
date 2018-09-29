c444f2d4808f58da514433f7b85d99905e92018c
Kyle <kderrick1988@gmail.com>
2018-09-28 17:31:54 -0700

    initial commit

---
+++ src/main/java/com/kyle/restwsclient/Restws1Application.java
@@ -0,0 +1,12 @@
+package com.kyle.restwsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restws1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restws1Application.class, args);
+	}
+}
